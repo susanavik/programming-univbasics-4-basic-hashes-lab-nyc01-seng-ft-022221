ef462e66fb83358361593051c23bb31c5e05e109
Susana vik <susana.m.vik@gmail.com>
2021-02-10 01:34:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,14 +7,15 @@ def my_hash
     :car => "Sam"
     :year => "2007"
   }
-  # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    :name => "Grace Hopper"
 end
 
 
 def id_hash_generator(number)
+  
   # return a hash with a key :id assigned to the provided number
 end
