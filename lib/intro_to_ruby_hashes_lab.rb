a3cc64d335af955e0387f31731c24c1cfdb5a0c5
Susana vik <susana.m.vik@gmail.com>
2021-02-10 01:36:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,9 @@ end
 
 
 def id_hash_generator(number)
-  
-  # return a hash with a key :id assigned to the provided number
+  id_hash_generator = {
+    :cars => 15
+    :planes => 25
+    }
+  :car.object_id
 end
